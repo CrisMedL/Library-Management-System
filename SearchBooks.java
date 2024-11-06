9807aeb8b1cc19c1548f5d28c0e2ed8b9a294685
Cristhian Medal <crist.medal@gmail.com>
2024-11-06 16:09:50 -0600

    Refactored Main.java logic and updated book management functionality

--- SearchBooks.java
+++ SearchBooks.java
@@ -1,28 +1,30 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class SearchBooks {
-    public static void searchBooks(ArrayList<Books> books){
-        Scanner sc = new Scanner(System.in);
+    public static void searchBooks(ArrayList<Books> books, Scanner sc){
         while (true) {
-        System.out.println("Enter the book information (title, author or year): ");
-        String identifier = sc.nextLine().toLowerCase();
+            System.out.println("Enter the book information (title, author or year): ");
+            String identifier = sc.nextLine().toLowerCase();
 
-        ArrayList<Books> bookMatches = new ArrayList<Books>();
-            for(Books book : books){
+            ArrayList<Books> bookMatches = new ArrayList<Books>();
+            for (Books book : books){
                 if (book.getTitle().toLowerCase().contains(identifier) ||
                     book.getAuthor().toLowerCase().contains(identifier) ||
                     String.valueOf(book.getYear()).contains(identifier)) {
-                        bookMatches.add(book);}}
-                if(bookMatches.isEmpty()){
-                    System.out.println("No matching books.");
-                } else {
-                    System.out.println("\nMatched books found: \n");
-                    for (Books book : bookMatches){
-                        System.out.println("ID: " + book.getID() + "\nTitle: '" + book.getTitle() + "'\nAuthor: " + book.getAuthor() + "\nYear: " + book.getYear() +"\n");
-                        break;}}
-                break;
+                        bookMatches.add(book);
+                }
+            }
+
+            if (bookMatches.isEmpty()){
+                System.out.println("No matching books.");
+            } else {
+                System.out.println("\nMatched books found: \n");
+                for (Books book : bookMatches){
+                    System.out.println("ID: " + book.getID() + "\nTitle: '" + book.getTitle() + "'\nAuthor: " + book.getAuthor() + "\nYear: " + book.getYear() + "\n");
+                }
+            }
+            break;  // Exit after one search query
         }
     }
 }
